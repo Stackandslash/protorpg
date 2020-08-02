e14e5a96d0e80a8e43646e21d7e2bc6fc3bf4d27
Haven <stackandslash@gmail.com>
2020-08-02 15:22:34 -0700

    package oddities.

---
+++ src/components/textentry/index.js
@@ -0,0 +1,11 @@
+import Cipher from "../cryptifyer";
+
+function Textentry(){ //pass text here as a prop
+    let text = "Hello Whirled"
+
+    return(
+    <h1>{text}</h1>
+    )
+}
+
+export default Textentry
